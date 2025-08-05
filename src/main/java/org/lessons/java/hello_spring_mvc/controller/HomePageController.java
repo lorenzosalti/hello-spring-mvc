c8ecf99ce36eb650eea48f3975a110894eee901a
lorenzosalti <lorenzosalti@gmail.com>
2025-08-05 15:55:29 +0200

    add: controller for home page

---
+++ src/main/java/org/lessons/java/hello_spring_mvc/controller/HomePageController.java
@@ -0,0 +1,10 @@
+package org.lessons.java.hello_spring_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+
+}
