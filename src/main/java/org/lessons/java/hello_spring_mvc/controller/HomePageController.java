0e3d13b8edd2d56e0ecf3685377502582c7768f5
lorenzosalti <lorenzosalti@gmail.com>
2025-08-05 17:13:50 +0200

    add: detail page with dinamic parameters from request and path

--- src/main/java/org/lessons/java/hello_spring_mvc/controller/HomePageController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controller/HomePageController.java
@@ -1,8 +1,11 @@
 package org.lessons.java.hello_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +14,13 @@ public class HomePageController {
   @GetMapping("/")
   public String homePage() {
     return "home";
+  }
+
+  @GetMapping("/detail/{id}")
+  public String detailPage(@PathVariable("id") String pageId, @RequestParam(name = "name") String name, Model model) {
+    model.addAttribute("pageId", pageId);
+    model.addAttribute("name", name);
+    return "detail";
   }
 
 }
